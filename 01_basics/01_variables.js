cae60ab0ea0730967b9de64f353bf898c15e55aa
Meet Dodiya <155138458+meet-dodiya1710@users.noreply.github.com>
2025-08-25 07:01:31 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144521
+let accountEmail = "abc@gmail.com"
+var accountPassword = "1234"
+accountCity = "Rajkot"
+let accountState;
+
+// accountId = 2  // is not allowed because we can't change the const value
+
+accountEmail = "meet@gmail.com"
+accountPassword = "5678"
+accountCity = "Bhavnagar"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// we can use a console.table for calling a multiple varialbles 
+// and if we want to call a single variable at a time than we can use console.log
